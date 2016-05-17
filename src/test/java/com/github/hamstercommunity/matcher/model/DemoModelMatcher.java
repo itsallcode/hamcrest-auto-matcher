a673c82b946ccf935bad7c69f3c479bdd644173b
kaklakariada <kaklakariada@users.noreply.github.com>
2016-05-17 08:57:12 +0200

    Use ConfigurableMatcher directly instead of extending it

--- src/test/java/com/github/hamstercommunity/matcher/model/DemoModelMatcher.java
+++ src/test/java/com/github/hamstercommunity/matcher/model/DemoModelMatcher.java
@@ -17,6 +17,8 @@
  */
 package com.github.hamstercommunity.matcher.model;
 
+import org.hamcrest.Matcher;
+
 import com.github.hamstercommunity.matcher.config.ConfigurableMatcher;
 import com.github.hamstercommunity.matcher.config.MatcherConfig;
 
@@ -24,20 +26,17 @@ import com.github.hamstercommunity.matcher.config.MatcherConfig;
  * A matcher for {@link DemoModel} used for testing {@link ConfigurableMatcher}
  * and demonstrating its usage.
  */
-public class DemoModelMatcher extends ConfigurableMatcher<DemoModel> {
+public class DemoModelMatcher {
 
-	private DemoModelMatcher(DemoModel expected) {
-		super(MatcherConfig.builder(expected) //
+	public static Matcher<DemoModel> equalTo(DemoModel expected) {
+		final MatcherConfig<DemoModel> config = MatcherConfig.builder(expected) //
 				.addEqualsProperty("id", DemoModel::getId) //
 				.addEqualsProperty("longVal", DemoModel::getLongVal) //
 				.addEqualsProperty("name", DemoModel::getName) //
 				.addProperty("attr", DemoModel::getAttr, DemoAttributeMatcher::equalTo) //
 				.addEqualsProperty("stringArray", DemoModel::getStringArray)
 				.addIterableProperty("children", DemoModel::getChildren, DemoModelMatcher::equalTo) //
-				.build());
-	}
-
-	public static DemoModelMatcher equalTo(DemoModel expected) {
-		return new DemoModelMatcher(expected);
+				.build();
+		return new ConfigurableMatcher<>(config);
 	}
 }
