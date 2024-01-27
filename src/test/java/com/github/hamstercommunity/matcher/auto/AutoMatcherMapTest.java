f09a56e9b7d98ccbdef7616c7cea4ce0aa038c05
kaklakariada <kaklakariada@users.noreply.github.com>
2024-01-27 15:16:48 +0100

    Remove license header

--- src/test/java/com/github/hamstercommunity/matcher/auto/AutoMatcherMapTest.java
+++ src/test/java/com/github/hamstercommunity/matcher/auto/AutoMatcherMapTest.java
@@ -1,27 +1,8 @@
-/**
- * Automatic hamcrest matcher for model classes
- * Copyright (C) 2017 Christoph Pirkl <christoph at users.sourceforge.net>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package com.github.hamstercommunity.matcher.auto;
 
 import static com.github.hamstercommunity.matcher.auto.TestUtil.assertValuesDoNotMatch;
 import static com.github.hamstercommunity.matcher.auto.TestUtil.assertValuesMatch;
-import static java.util.Collections.emptyMap;
-import static java.util.Collections.singletonList;
-import static java.util.Collections.singletonMap;
+import static java.util.Collections.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.HashMap;
